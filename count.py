f9070e0ba5beee9bda29fedae3acfb70deddc2f3
chukwuemekaajah <talk2ajah@gmail.com>
2020-05-30 20:57:08 +0100

    maded changes to function docstrings and code formatting

--- count.py
+++ count.py
@@ -1,6 +1,6 @@
 def count_lines(file, flags = {}):
 	"""
-		a function to count the number of lines in a file based on absence or presence of some flags
+		returns the number of lines of code in a python file
 		:param file: - string file name representing the file full address
 		:param flags: - a dictionary of conditions to note while reading the file
 		:param flags.no_comments: - a flag to remove comments from the counting
@@ -8,6 +8,7 @@ def count_lines(file, flags = {}):
 		:return int: number of lines in the code file
 	"""
 	number_of_lines = 0
+
 	try:
 		file_object = open(file, 'r')
 	except IOError as err:
@@ -31,39 +32,41 @@ def count_lines(file, flags = {}):
 			print("number of lines after removing docs", len(file_lines))
 		
 		number_of_lines = len(file_lines)
-		print("number of lines is ", number_of_lines)
 		return number_of_lines
 
 def is_not_empty(file_line):
 	"""
-		check if the line is not an empty line 
+		returns True if a file is not empty otherwise False
 		:param file_line: a string of characters representing a line in a file
 		:return bool
 	"""
 	stripped_line = file_line.strip()
 	if bool(stripped_line) is False:
 		return False
+
 	return True
 
 def is_not_comment(file_line):
 	"""
-		check if the file line is a python comment
+		returns True if the file line is not a comment otherwise False
 		:param file_line: a string of characters representing a line in a file
 		:return bool
 	"""
 	stripped_line = file_line.strip()
 	if stripped_line.startswith("#"):
 		return False
+
 	return True
 
 def remove_docs(file_lines):
 	'''
-		remove documentation lines in class and function bodies
+		returns a list of file lines without docstrings
 		:param file_lines: a list of strings representing a line in a file
 		:return list: a list of strings representing lines in the list that are not documentation text
 	'''
 	doc_start_line = []
 	doc_end_line = []
+
 	for line_number, line in enumerate(file_lines):
 		stripped_line = line.strip()
 		# check for double quotes version
@@ -81,7 +84,7 @@ def remove_docs(file_lines):
 			doc_start_line.append(line_number)
 
 	file_without_docs = []
-	counter = 0
+
 	for i in range(len(doc_start_line)):
 		if i == 0:
 			file_without_docs.extend(file_lines[0:doc_start_line[i]])
@@ -89,6 +92,7 @@ def remove_docs(file_lines):
 			file_without_docs.extend(file_lines[doc_end_line[i]+1:doc_start_line[i+1]])
 		else:
 			file_without_docs.extend(file_lines[doc_end_line[len(doc_start_line) - 1]+1:])
+
 	return file_without_docs	
 
 count_lines("count.py", {"no_docs":True, "no_comments":True})
