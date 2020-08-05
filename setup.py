cebee36b5e618a7c7fd28b72067bc1971295d8cf
chukwuemekaajah <talk2ajah@gmail.com>
2020-08-06 00:34:55 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='line_count', version='0.1', description='A utility package to enable developers count the actual number of lines of code they have written for a project.', url='https://github.com/ChukwuEmekaAjah/https://github.com/ChukwuEmekaAjah/line-count', author='Chukwuemeka Ajah', author_email='talk2ajah@gmail.com', license='MIT', packages=['line_count'], zip_safe=True)
